8efd460ea108c186e3e6006ee3684853fbc2212a
coolfeature <szymonpiotrczaja@gmail.com>
2016-05-26 10:43:40 +0100

    Editor.prototype.load fixed

--- js/drooky/Editor.js
+++ js/drooky/Editor.js
@@ -53,9 +53,10 @@
 	Editor.prototype.load = function(fn,args) {
 		fn.prototype = Object.create(DROOKY.Editor.Command.prototype);
 		fn.prototype.constructor = fn;
-		var inst = new fn;
+		var inst = Object.create(fn.prototype);
+		fn.apply(inst,args)
 		inst.init();
-		return fn.apply(inst,args);
+		return inst;
 	}	
 	
 })(DROOKY);
